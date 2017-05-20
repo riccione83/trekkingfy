7e8db8faa68e775891e329cdf48572a719b6beb4
Riccardo Rizzo <r.riki@tiscali.it>
2017-05-20 09:55:12 +0200

    Initial Commit

---
+++ Trekkingfy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trekkingfy
+//
+//  Created by Riccardo Rizzo on 20/05/17.
+//  Copyright © 2017 Riccardo Rizzo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

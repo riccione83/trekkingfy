73799faeaa8051f7171293e45cc0248beaeacd86
Riccardo Rizzo <r.riki@tiscali.it>
2017-07-04 17:07:06 +0200

    Added new photo icon and fixed rotate screen

--- Trekkingfy/PhotoCameraViewController.swift
+++ Trekkingfy/PhotoCameraViewController.swift
@@ -24,12 +24,17 @@ class PhotoCameraViewController: UIViewController {
     @IBOutlet var btnTakePhoto: UIButton!
     @IBOutlet var btnOK: UIButton!
     @IBOutlet var txtNote: UITextField!
+    @IBOutlet var photoBackgroundButton: UIView!
+    
     
     var mainViewDelegate: PhotoShootDelegate?
     var currentID:Int?
     
     var boxView:UIView!
     let myButton: UIButton = UIButton()
+    
+    
+    var imageOrientation = UIImageOrientation.right
     
     var session: AVCaptureSession?
     var stillImageOutput: AVCaptureStillImageOutput?
@@ -50,6 +55,7 @@ class PhotoCameraViewController: UIViewController {
     }
     
     @IBAction func takePhoto(_ sender: Any) {
+        
         if let videoConnection = stillImageOutput!.connection(withMediaType: AVMediaTypeVideo) {
             
             stillImageOutput?.captureStillImageAsynchronously(from: videoConnection, completionHandler: { (sampleBuffer, error) -> Void in
@@ -59,7 +65,8 @@ class PhotoCameraViewController: UIViewController {
                     let dataProvider = CGDataProvider(data: imageData! as CFData)
                     let cgImageRef = CGImage(jpegDataProviderSource: dataProvider!, decode: nil, shouldInterpolate: true, intent: CGColorRenderingIntent.defaultIntent)
                     
-                    let image = UIImage(cgImage: cgImageRef!, scale: 1.0, orientation: UIImageOrientation.right)
+                    let image = UIImage(cgImage: cgImageRef!, scale: 1.0, orientation: self.imageOrientation)
+                    //UIImageOrientation.right
                     
                     self.videoPreviewLayer?.removeFromSuperlayer()
                     self.captureImageView.image = image
@@ -99,6 +106,52 @@ class PhotoCameraViewController: UIViewController {
         self.present(alert, animated: true, completion: nil)
     }
     
+    private func updatePreviewLayer(layer: AVCaptureConnection, orientation: AVCaptureVideoOrientation) {
+        
+        layer.videoOrientation = orientation
+        
+        self.videoPreviewLayer?.frame = self.view.bounds
+        
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        if let connection =  self.videoPreviewLayer?.connection  {
+            
+            let currentDevice: UIDevice = UIDevice.current
+            
+            let orientation: UIDeviceOrientation = currentDevice.orientation
+            
+            let previewLayerConnection : AVCaptureConnection = connection
+            
+            if previewLayerConnection.isVideoOrientationSupported {
+                
+                switch (orientation) {
+                case .portrait: updatePreviewLayer(layer: previewLayerConnection, orientation: .portrait)
+                    imageOrientation = UIImageOrientation.right
+                    break
+                    
+                case .landscapeRight: updatePreviewLayer(layer: previewLayerConnection, orientation: .landscapeLeft)
+                    imageOrientation = UIImageOrientation.down
+                    break
+                    
+                case .landscapeLeft: updatePreviewLayer(layer: previewLayerConnection, orientation: .landscapeRight)
+                imageOrientation = UIImageOrientation.up
+                    break
+                    
+                case .portraitUpsideDown: updatePreviewLayer(layer: previewLayerConnection, orientation: .portraitUpsideDown)
+                imageOrientation = UIImageOrientation.down
+                    break
+                    
+                default: updatePreviewLayer(layer: previewLayerConnection, orientation: .portrait)
+                
+                    break
+                }
+            }
+        }
+    }
+    
     private func startPhotoCamera() {
         
         btnOK.isHidden = true
@@ -111,14 +164,17 @@ class PhotoCameraViewController: UIViewController {
         
         var error: NSError?
         var input: AVCaptureDeviceInput!
+        
         do {
             input = try AVCaptureDeviceInput(device: backCamera)
-        } catch let error1 as NSError {
+            
+        }
+        catch let error1 as NSError {
             error = error1
             input = nil
             print(error!.localizedDescription)
-            
         }
+        
         if error == nil && session!.canAddInput(input) {
             session!.addInput(input)
             
@@ -129,8 +185,12 @@ class PhotoCameraViewController: UIViewController {
                 session!.addOutput(stillImageOutput)
                 videoPreviewLayer = AVCaptureVideoPreviewLayer(session: session)
                 videoPreviewLayer!.videoGravity = AVLayerVideoGravityResizeAspect
-                videoPreviewLayer!.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
+                //videoPreviewLayer!.connection?.videoOrientation = AVCaptureVideoOrientation.portrait
                 previewView.layer.addSublayer(videoPreviewLayer!)
+                
+                previewView.bringSubview(toFront: photoBackgroundButton)
+                previewView.bringSubview(toFront: btnTakePhoto)
+                previewView.bringSubview(toFront: txtNote)
                 session!.startRunning()
             }
             
